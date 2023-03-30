895c866099fac133626dcaec59ab1535af9096a5
Danis Alukaev <d.alukaev@innopolis.university>
2023-03-30 10:00:48 +0000

    #36 + intermediate results

--- autoconcept/extract.py
+++ autoconcept/extract.py
@@ -45,7 +45,7 @@ def main(cfg: DictConfig):
     vocab_size = len(dm.dataloader_kwargs['collate_fn'].vocabulary.vocab)
     print(f"Vocab size: {vocab_size}")
 
-    checkpoint_path = "/home/danis/Projects/AlphaCaption/AutoConceptBottleneck/autoconcept/outputs/2023-03-30/05-25-37/lightning_logs/version_0/checkpoints/epoch021-val_loss0.28261.ckpt"
+    checkpoint_path = "/home/danis/Projects/AlphaCaption/AutoConceptBottleneck/autoconcept/outputs/2023-03-30/09-20-11/lightning_logs/version_0/checkpoints/last.ckpt"
     target_class = get_class(cfg.model._target_)
     main = instantiate(cfg.model.main)
     inference = target_class.load_from_checkpoint(
