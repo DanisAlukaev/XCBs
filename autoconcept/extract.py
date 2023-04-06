95e620cdc9a4061ff7da18975477b774532b84a1
Danis Alukaev <d.alukaev@innopolis.university>
2023-04-06 19:00:20 +0000

    #36 + softmax norm in slot attention

--- autoconcept/extract.py
+++ autoconcept/extract.py
@@ -46,7 +46,7 @@ def main(cfg: DictConfig):
     vocab_size = len(dm.dataloader_kwargs['collate_fn'].vocabulary.vocab)
     print(f"Vocab size: {vocab_size}")
 
-    checkpoint_path = "/home/danis/Projects/AlphaCaption/AutoConceptBottleneck/autoconcept/outputs/2023-04-06/17-55-01/lightning_logs/version_0/checkpoints/last.ckpt"
+    checkpoint_path = "/home/danis/Projects/AlphaCaption/AutoConceptBottleneck/autoconcept/outputs/2023-04-06/18-41-58/lightning_logs/version_0/checkpoints/last.ckpt"
     target_class = get_class(cfg.model._target_)
     main = instantiate(cfg.model.main)
     inference = target_class.load_from_checkpoint(
